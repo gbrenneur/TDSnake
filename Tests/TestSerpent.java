f444ebf416f66636a64d8ca92e5c695978877499
Benoufella.hamida <Benoufella.hamida@gmail.com>
2013-09-20 17:31:29 +0200

    commit test

---
+++ Tests/TestSerpent.java
@@ -0,0 +1,17 @@
+import org.junit.*;
+
+public class TestSerpent {
+	@Before
+	public void initialisationTest() {
+		// la list
+		// deux positions
+
+	}
+
+	@Test
+	private void testPeutAvancer() {
+		//  TODO a completer
+
+	}
+
+}
